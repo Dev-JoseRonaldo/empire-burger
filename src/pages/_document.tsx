263b0cd466d7f51ce613e28bde916b76cd8879e8
José Ronaldo <dev.joseronaldo@gmail.com>
2022-10-19 23:35:51 -0300

    chore: add custom font and iconify dependencies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,8 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Lilita+One&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Lilita+One&display=swap"
             rel="stylesheet"
           />
         </Head>
